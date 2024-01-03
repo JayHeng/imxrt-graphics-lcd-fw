d1d26727992281563c42db28e53ede9961cb2c63
Jay Heng <hengjie1989@foxmail.com>
2024-01-03 21:29:58 +0800

    [SDK] Components: video/ili9806e #3
    - Set param according to .dts which was verified on RK3588S
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- components/video/display/ili9806e/fsl_ili9806e.c
+++ components/video/display/ili9806e/fsl_ili9806e.c
@@ -62,98 +62,73 @@ static const struct ili9806e_msg panel_init[] = {
 	 *   SDO_STATUS = 1 (always output, but without output tri-state)
 	 */
 	ILI9806_SET_REG_PARAM(0x08, 0x10),
-	/* display control
-	 * VSPL = 1 (vertical sync polarity)
+	/* bit0 - sync mode
+	 * 0: DE mode
+	 * 1: SYNC mode
+	 */
+	ILI9806_SET_REG_PARAM(0x20, 0x00),
+	/* bit[3:0] - display control
+	 * VSPL = 0 (vertical sync polarity)
 	 * HSPL = 0 (horizontal sync polarity)
 	 * DPL = 0 (PCLK polarity)
 	 * EPL = 1 (data enable polarity)
 	 */
-	ILI9806_SET_REG_PARAM(0x21, 0x0d),
-	/* resolution control (0x02 = 480x800) */
-	ILI9806_SET_REG_PARAM(0x30, 0x02),
+	ILI9806_SET_REG_PARAM(0x21, 0x01),
+	/* resolution control (0x01 = 480x854) */
+	ILI9806_SET_REG_PARAM(0x30, 0x01),
 	/* display inversion control (0x00 = column inversion) */
 	ILI9806_SET_REG_PARAM(0x31, 0x00),
-	/* power control
-	 *  EXB1T = 0 (internal charge pump)
-	 *  EXT_CPCK_SEL = 1 (pump clock control signal = output 2 x waveform)
-	 *  BT = 0 (DDVDH / DDVDL voltage = VCI x 2 / VCI x -2)
-	 */
-	ILI9806_SET_REG_PARAM(0x40, 0x10),
-	/* power control
-	 *  DDVDH_CLP = 5.6 (DDVDH clamp leve)
-	 *  DDVDL_CLP = -5.6 (DDVDL clamp leve)
-	 */
-	ILI9806_SET_REG_PARAM(0x41, 0x55),
-	/* power control
-	 *  VGH_CP = 2DDVDH - DDVDL (step up factor for VGH)
-	 *  VGL_CP = DDVDL + VCL - VCIP (step up factor for VGL)
-	 */
-	ILI9806_SET_REG_PARAM(0x42, 0x02),
-	/* power control
-	 *  VGH_CLPEN = 0 (disable VGH clamp level)
-	 *  VGH_CLP = 9 (15.0 VGH clamp level - but this is disabled so not used?)
-	 */
-	ILI9806_SET_REG_PARAM(0x43, 0x84),
-	/* power control
-	 *  VGL_CLPEN = 0 (disable VGL clamp level)
-	 *  VGL_CLP = 9 (-11.0 VGL clamp level - but this is disabled so not used?)
-	 */
-	ILI9806_SET_REG_PARAM(0x44, 0x84),
-
-	/* power control
-	 *  VREG1OUT voltage for positive gamma?
-	 */
-	ILI9806_SET_REG_PARAM(0x50, 0x78),
-	/* power control
-	 *  VREG2OUT voltage for negative gamma?
-	 */
-	ILI9806_SET_REG_PARAM(0x51, 0x78),
-
+	ILI9806_SET_REG_PARAM(0x40, 0x16),
+	ILI9806_SET_REG_PARAM(0x41, 0x33),
+	ILI9806_SET_REG_PARAM(0x42, 0x03),
+	ILI9806_SET_REG_PARAM(0x43, 0x09),
+	ILI9806_SET_REG_PARAM(0x44, 0x06),
+	ILI9806_SET_REG_PARAM(0x50, 0x88),
+	ILI9806_SET_REG_PARAM(0x51, 0x88),
 	ILI9806_SET_REG_PARAM(0x52, 0x00),
-	ILI9806_SET_REG_PARAM(0x53, 0x77),
-	ILI9806_SET_REG_PARAM(0x57, 0x60),
+	ILI9806_SET_REG_PARAM(0x53, 0x49),
+	ILI9806_SET_REG_PARAM(0x55, 0x49),
 	ILI9806_SET_REG_PARAM(0x60, 0x07),
 	ILI9806_SET_REG_PARAM(0x61, 0x00),
-	ILI9806_SET_REG_PARAM(0x62, 0x08),
+	ILI9806_SET_REG_PARAM(0x62, 0x07),
 	ILI9806_SET_REG_PARAM(0x63, 0x00),
 	ILI9806_SET_REG_PARAM(0xA0, 0x00),
-	ILI9806_SET_REG_PARAM(0xA1, 0x07),
-	ILI9806_SET_REG_PARAM(0xA2, 0x0C),
+	ILI9806_SET_REG_PARAM(0xA1, 0x09),
+	ILI9806_SET_REG_PARAM(0xA2, 0x11),
 	ILI9806_SET_REG_PARAM(0xA3, 0x0B),
-	ILI9806_SET_REG_PARAM(0xA4, 0x03),
-	ILI9806_SET_REG_PARAM(0xA5, 0x07),
+	ILI9806_SET_REG_PARAM(0xA4, 0x05),
+	ILI9806_SET_REG_PARAM(0xA5, 0x08),
 	ILI9806_SET_REG_PARAM(0xA6, 0x06),
 	ILI9806_SET_REG_PARAM(0xA7, 0x04),
-	ILI9806_SET_REG_PARAM(0xA8, 0x08),
+	ILI9806_SET_REG_PARAM(0xA8, 0x09),
 	ILI9806_SET_REG_PARAM(0xA9, 0x0C),
-	ILI9806_SET_REG_PARAM(0xAA, 0x13),
-	ILI9806_SET_REG_PARAM(0xAB, 0x06),
-	ILI9806_SET_REG_PARAM(0xAC, 0x0D),
-	ILI9806_SET_REG_PARAM(0xAD, 0x19),
-	ILI9806_SET_REG_PARAM(0xAE, 0x10),
+	ILI9806_SET_REG_PARAM(0xAA, 0x15),
+	ILI9806_SET_REG_PARAM(0xAB, 0x08),
+	ILI9806_SET_REG_PARAM(0xAC, 0x0F),
+	ILI9806_SET_REG_PARAM(0xAD, 0x12),
+	ILI9806_SET_REG_PARAM(0xAE, 0x09),
 	ILI9806_SET_REG_PARAM(0xAF, 0x00),
 	/* negative gamma control
 	 * set the gray scale voltage to adjust the gamma characteristics of the panel
 	 */
 	ILI9806_SET_REG_PARAM(0xC0, 0x00),
-	ILI9806_SET_REG_PARAM(0xC1, 0x07),
-	ILI9806_SET_REG_PARAM(0xC2, 0x0C),
-	ILI9806_SET_REG_PARAM(0xC3, 0x0B),
-	ILI9806_SET_REG_PARAM(0xC4, 0x03),
-	ILI9806_SET_REG_PARAM(0xC5, 0x07),
-	ILI9806_SET_REG_PARAM(0xC6, 0x07),
+	ILI9806_SET_REG_PARAM(0xC1, 0x09),
+	ILI9806_SET_REG_PARAM(0xC2, 0x10),
+	ILI9806_SET_REG_PARAM(0xC3, 0x0C),
+	ILI9806_SET_REG_PARAM(0xC4, 0x05),
+	ILI9806_SET_REG_PARAM(0xC5, 0x08),
+	ILI9806_SET_REG_PARAM(0xC6, 0x06),
 	ILI9806_SET_REG_PARAM(0xC7, 0x04),
 	ILI9806_SET_REG_PARAM(0xC8, 0x08),
 	ILI9806_SET_REG_PARAM(0xC9, 0x0C),
-	ILI9806_SET_REG_PARAM(0xCA, 0x13),
-	ILI9806_SET_REG_PARAM(0xCB, 0x06),
-	ILI9806_SET_REG_PARAM(0xCC, 0x0D),
-	ILI9806_SET_REG_PARAM(0xCD, 0x18),
-	ILI9806_SET_REG_PARAM(0xCE, 0x10),
+	ILI9806_SET_REG_PARAM(0xCA, 0x14),
+	ILI9806_SET_REG_PARAM(0xCB, 0x08),
+	ILI9806_SET_REG_PARAM(0xCC, 0x0F),
+	ILI9806_SET_REG_PARAM(0xCD, 0x11),
+	ILI9806_SET_REG_PARAM(0xCE, 0x09),
 	ILI9806_SET_REG_PARAM(0xCF, 0x00),
 
 	ILI9806_SET_PAGE(6),
-
 	ILI9806_SET_REG_PARAM(0x00, 0x20),
 	ILI9806_SET_REG_PARAM(0x01, 0x0A),
 	ILI9806_SET_REG_PARAM(0x02, 0x00),
@@ -168,7 +143,7 @@ static const struct ili9806e_msg panel_init[] = {
 	ILI9806_SET_REG_PARAM(0x0B, 0x00),
 	ILI9806_SET_REG_PARAM(0x0C, 0x01),
 	ILI9806_SET_REG_PARAM(0x0D, 0x01),
-	ILI9806_SET_REG_PARAM(0x0E, 0x00),
+	ILI9806_SET_REG_PARAM(0x0E, 0x05),
 	ILI9806_SET_REG_PARAM(0x0F, 0x00),
 	ILI9806_SET_REG_PARAM(0x10, 0xF0),
 	ILI9806_SET_REG_PARAM(0x11, 0xF4),
@@ -209,30 +184,20 @@ static const struct ili9806e_msg panel_init[] = {
 	ILI9806_SET_REG_PARAM(0x3E, 0x22),
 	ILI9806_SET_REG_PARAM(0x3F, 0x22),
 	ILI9806_SET_REG_PARAM(0x40, 0x22),
-	/* register doesn't exist on page 6? */
-	ILI9806_SET_REG_PARAM(0x52, 0x10),
-	/* doesn't make sense, not valid according to datasheet */
-	ILI9806_SET_REG_PARAM(0x53, 0x10),
-	/* doesn't make sense, not valid according to datasheet */
-	ILI9806_SET_REG_PARAM(0x54, 0x13),
 
 	ILI9806_SET_PAGE(7),
-
-	/* enable VREG */
-	ILI9806_SET_REG_PARAM(0x18, 0x1D),
 	/* enable VGL_REG */
 	ILI9806_SET_REG_PARAM(0x17, 0x22),
 	/* register doesn't exist on page 7? */
 	ILI9806_SET_REG_PARAM(0x02, 0x77),
 	/* register doesn't exist on page 7? */
 	ILI9806_SET_REG_PARAM(0x26, 0xB2),
-	/* register doesn't exist on page 7? */
-	ILI9806_SET_REG_PARAM(0xE1, 0x79),
 
 	ILI9806_SET_PAGE(0),
-	ILI9806_SET_REG_PARAM(MIPI_DCS_SET_PIXEL_FORMAT, MIPI_DCS_PIXEL_FMT_18BIT << 4),
-	ILI9806_SET_REG_PARAM(MIPI_DCS_SET_TEAR_ON, 0x00),
-	ILI9806_SET_REG(MIPI_DCS_EXIT_SLEEP_MODE),
+    ILI9806_SET_REG(0x35),
+	ILI9806_SET_REG_PARAM(0x3A, 0x70),
+	ILI9806_SET_REG(0x11),
+	ILI9806_SET_REG(0x29),
 };
 
 #define NUM_INIT_REGS ARRAY_SIZE(panel_init)
@@ -258,11 +223,11 @@ status_t ILI9806E_Init(display_handle_t *handle, const display_config_t *config)
 
     /* Power on. */
     resource->pullPowerPin(true);
-    ILI9806E_DelayMs(1);
+    ILI9806E_DelayMs(1U);
 
     /* Perform reset. */
     resource->pullResetPin(false);
-    ILI9806E_DelayMs(1);
+    ILI9806E_DelayMs(50U);
     resource->pullResetPin(true);
     ILI9806E_DelayMs(50U);
     
